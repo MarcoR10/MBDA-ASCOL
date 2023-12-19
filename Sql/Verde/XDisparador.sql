5a78dd8bd63ed95835a376cba101da24163f480a
MarcoR10 <marco2001ar@gmail.com>
2023-12-19 05:05:32 -0500

    TT

--- Sql/Verde/XDisparador.sql
+++ Sql/Verde/XDisparador.sql
@@ -1,1 +1,6 @@
-DROP TRIGGER trg_actualizar_equipo;
+DROP TRIGGER AsignarArbitroEspañol;
+DROP TRIGGER trg_control_asistencia_estadio;
+DROP TRIGGER trg_evitar_eliminacion_estadio;
+DROP TRIGGER trg_actualizar_puntos_equipo;
+DROP TRIGGER trg_actualizar_fecha_fundacion_equipo;
+DROP TRIGGER trg_control_eliminar_arbitro;
